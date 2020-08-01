9f5a74bdb011f77c821f1b25e39fe29aefecaf1b
Jose <joalloce@gmail.com>
2020-07-31 23:07:56 -0600

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Box, Typography, Container } from "@material-ui/core";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h3" component="h1">
+          AOE2 API
+        </Typography>
+      </Box>
+    </Container>
   );
 }
 
